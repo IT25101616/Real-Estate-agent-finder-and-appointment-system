5d078b2fba6fd005b29f9ee660bfe7c8f357d1cc
Thisara Dewdunu <it25101616@my.sliit.lk>
2026-05-13 17:09:23 +0530

    Remove files not related to my part

--- src/main/java/com/realestate/repository/PropertyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.realestate.repository;
-
-import com.realestate.model.Property;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PropertyRepository extends JpaRepository<Property, Integer> {
-}
