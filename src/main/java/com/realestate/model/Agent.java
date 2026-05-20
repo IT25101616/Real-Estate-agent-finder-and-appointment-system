9110f445cdda3fbf4cb1de51c6214ff816d9e6fb
IT25103217 <it25103217@my.sliit.lk>
2026-05-20 21:42:41 +0530

    Agent.java

--- src/main/java/com/realestate/model/Agent.java
+++ src/main/java/com/realestate/model/Agent.java
@@ -1,9 +1,11 @@
 package com.realestate.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
-public class Agent extends User {
+@Table(name = "agents")
+public class Agent extends Person {
 
     private String phone;
     private String area;
@@ -12,37 +14,36 @@ public class Agent extends User {
     public Agent() {
     }
 
-    public Agent(String name,
-                 String email,
-                 String phone,
-                 String area,
-                 int experienceYears) {
-
-        super(0, name, email);
-
+    public Agent(String name, String email, String phone, String area, int experienceYears) {
+        super(name, email);
         this.phone = phone;
         this.area = area;
         this.experienceYears = experienceYears;
+    }
+
+    @Override
+    public String getRoleName() {
+        return "Agent";
     }
 
     public String getPhone() {
         return phone;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
     public String getArea() {
         return area;
     }
 
-    public void setArea(String area) {
-        this.area = area;
-    }
-
     public int getExperienceYears() {
         return experienceYears;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
     }
 
     public void setExperienceYears(int experienceYears) {
