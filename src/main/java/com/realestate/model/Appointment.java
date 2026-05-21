e12aaa09307f3941dbed1fa621155cbc2d553305
IT25102584 <it25102584@my.sliit.lk>
2026-05-21 12:53:46 +0530

    Appointment

--- src/main/java/com/realestate/model/Appointment.java
+++ src/main/java/com/realestate/model/Appointment.java
@@ -14,19 +14,22 @@ public class Appointment extends ServiceRecord{
     private String clientEmail;
     private String phoneNumber;
     private String appointmentDate;
+    private String message;
 
 
     public Appointment() {
     }
 
-    public Appointment(String createdBy, String clientName, String clientEmail, String phoneNumber, String appointmentDate) {
+    public Appointment(String createdBy, String clientName, String clientEmail, String phoneNumber, String appointmentDate,String message) {
         super(createdBy);
         this.clientName = clientName;
         this.clientEmail = clientEmail;
         this.phoneNumber = phoneNumber;
         this.appointmentDate = appointmentDate;
+        this.message = message;
     }
 
+    @Override
     public String getRecordType() {
         return "Appointment Record";
     }
@@ -51,6 +54,14 @@ public class Appointment extends ServiceRecord{
         return appointmentDate;
     }
 
+    public String getMessage() {
+        return message;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public void setClientName(String clientName) {
         this.clientName = clientName;
     }
@@ -65,5 +76,9 @@ public class Appointment extends ServiceRecord{
 
     public void setAppointmentDate(String appointmentDate) {
         this.appointmentDate = appointmentDate;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
