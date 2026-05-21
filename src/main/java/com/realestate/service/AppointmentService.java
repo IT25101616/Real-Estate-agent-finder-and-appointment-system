bfadd2db347396f3a815d0664f588b6f313be85f
IT25102584 <it25102584@my.sliit.lk>
2026-05-21 17:52:16 +0530

    Appointment

--- src/main/java/com/realestate/service/AppointmentService.java
+++ src/main/java/com/realestate/service/AppointmentService.java
@@ -4,10 +4,8 @@ import com.realestate.model.Appointment;
 import com.realestate.repository.AppointmentRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class AppointmentService implements ManagementService<Appointment> {
+public class AppointmentService {
 
     private final AppointmentRepository appointmentRepository;
 
@@ -15,46 +13,7 @@ public class AppointmentService implements ManagementService<Appointment> {
         this.appointmentRepository = appointmentRepository;
     }
 
-    @Override
-    public Appointment create(Appointment appointment) {
+    public Appointment saveAppointment(Appointment appointment) {
         return appointmentRepository.save(appointment);
-    }
-
-    @Override
-    public List<Appointment> getAll() {
-        return appointmentRepository.findAll();
-    }
-
-    @Override
-    public Appointment getById(Long id) {
-        return appointmentRepository.findById(id).orElse(null);
-    }
-
-    @Override
-    public Appointment update(Long id, Appointment newAppointment) {
-        Appointment oldAppointment = appointmentRepository.findById(id).orElse(null);
-
-        if (oldAppointment != null) {
-            oldAppointment.setClientName(newAppointment.getClientName());
-            oldAppointment.setClientEmail(newAppointment.getClientEmail());
-            oldAppointment.setPhoneNumber(newAppointment.getPhoneNumber());
-            oldAppointment.setAppointmentDate(newAppointment.getAppointmentDate());
-            oldAppointment.setMessage(newAppointment.getMessage());
-            oldAppointment.setCreatedBy(newAppointment.getCreatedBy());
-
-            return appointmentRepository.save(oldAppointment);
-        }
-
-        return null;
-    }
-
-    @Override
-    public boolean delete(Long id) {
-        if (appointmentRepository.existsById(id)) {
-            appointmentRepository.deleteById(id);
-            return true;
-        }
-
-        return false;
     }
 }
