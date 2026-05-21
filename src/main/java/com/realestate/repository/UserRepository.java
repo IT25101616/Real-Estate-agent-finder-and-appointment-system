8b349186bebb672a4715d6e806f411c9fd6ca54a
IT25103521 <it25103521@my.sliit.lk>
2026-05-21 20:47:35 +0530

    Update UserRepository.java

--- src/main/java/com/realestate/repository/UserRepository.java
+++ src/main/java/com/realestate/repository/UserRepository.java
@@ -1,55 +1,9 @@
 package com.realestate.repository;
 
 import com.realestate.model.User;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
+public interface UserRepository extends JpaRepository<User, Long> {
 
-@Repository
-public class UserRepository {
-
-    private final ArrayList<User> users = new ArrayList<>();
-
-    public ArrayList<User> findAll() {
-        return users;
-    }
-
-    public User findById(int id) {
-
-        for (User user : users) {
-
-            if (user.getUserId() == id) {
-                return user;
-            }
-        }
-
-        return null;
-    }
-
-    public User save(User user) {
-
-        users.add(user);
-
-        return user;
-    }
-
-    public User update(int id, User newUser) {
-
-        User oldUser = findById(id);
-
-        if (oldUser != null) {
-
-            oldUser.setName(newUser.getName());
-            oldUser.setEmail(newUser.getEmail());
-        }
-
-        return oldUser;
-    }
-
-    public boolean delete(int id) {
-
-        User user = findById(id);
-
-        return users.remove(user);
-    }
+    User findByEmail(String email);
 }
