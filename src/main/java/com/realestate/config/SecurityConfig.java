54b3682296cd3f4e6d94d67e6a24d5d89998d7a1
IT25103521 <it25103521@my.sliit.lk>
2026-05-21 20:46:25 +0530

    Update SecurityConfig.java

--- src/main/java/com/realestate/config/SecurityConfig.java
+++ src/main/java/com/realestate/config/SecurityConfig.java
@@ -11,9 +11,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.disable())
+        http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll());
+                        .anyRequest().permitAll()
+                );
 
         return http.build();
     }
