2fdda0b2f155f2cdefc109f7c4d390d67ef6e4f9
IT25102584 <it25102584@my.sliit.lk>
2026-05-21 18:10:57 +0530

    Appointment Repository

--- src/main/java/com/realestate/repository/AppointmentRepository.java
+++ src/main/java/com/realestate/repository/AppointmentRepository.java
@@ -3,6 +3,5 @@ package com.realestate.repository;
 import com.realestate.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AppointmentRepository
-        extends JpaRepository<Appointment, Integer> {
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 }
