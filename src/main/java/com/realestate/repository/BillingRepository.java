f9b9cc44a6f54e5c60856fb8071dd7ada130cfdc
IT25101616 <IT25101616@my.sliit.lk>
2026-05-21 19:58:36 +0530

    Update BillingRepository.java

--- src/main/java/com/realestate/repository/BillingRepository.java
+++ src/main/java/com/realestate/repository/BillingRepository.java
@@ -1,13 +1,7 @@
-package com.realestate.billing;
+package com.realestate.repository;
 
+import com.realestate.model.Billing;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
-public interface BillingRepository extends JpaRepository<Billing, Integer> {
-
-    // Read bills by customer email
-    List<Billing> findByEmail(String email);
-
-    // Read bills by payment status. Example: PAID or PENDING
-    List<Billing> findByPaymentStatus(String paymentStatus);
+public interface BillingRepository extends JpaRepository<Billing, Long> {
 }
